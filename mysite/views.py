cc0b66450c441bb80ff963b4affa41677c9ce369
Julian Gil <juliangils@yahoo.com.co>
2014-06-20 03:56:18 -0500

    Se agregó funcion current-datetime en views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import datetime
 
 def hello(request): #request: Solicitud por convencion y es una instancia de a clase django.http.HttpRequest
     return HttpResponse("Hello world") #No hacemos nada con el request, pero siempre debe de llevar un parametro
+
+#FUNCION QUE RETORNA LA HORA DE NUESTRO SERVIDOR (CODIGO .PY)
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
